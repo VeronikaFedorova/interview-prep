21049d42c46fee6a3b758c9288f8957dbd5aa7bf
Veronika Fedorova <fedorovavv03@gmail.com>
2025-04-23 23:00:13 +0300

    Implemented Home Page

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,41 @@
 type FormType = 'sign-in' | 'sign-up';
+
+interface InterviewCardProps {
+  interviewId?: string;
+  userId?: string;
+  role: string;
+  type: string;
+  techstack: string[];
+  createdAt?: string;
+}
+
+interface Interview {
+  id: string;
+  userId: string;
+  role: string;
+  type: string;
+  level: string;
+  techstack: string[];
+  questions: string[];
+  finalized: boolean;
+  createdAt: string;
+}
+
+interface Feedback {
+  id: string;
+  interviewId: string;
+  totalScore: number;
+  categoryScores: Array<{
+    name: string;
+    score: number;
+    comment: string;
+  }>;
+  strengths: string[];
+  areasForImprovement: string[];
+  finalAssessment: string;
+  createdAt: string;
+}
+
+interface TechIconProps {
+  techStack: string[];
+}
