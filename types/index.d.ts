fbe82b3df3e558c7cebe92dbf654bf1a801595ae
Veronika Fedorova <fedorovavv03@gmail.com>
2025-04-25 12:47:35 +0300

    Implemented SignIn & SignUp functionality

--- types/index.d.ts
+++ types/index.d.ts
@@ -39,3 +39,21 @@ interface Feedback {
 interface TechIconProps {
   techStack: string[];
 }
+
+interface SignUpParams {
+  uid: string;
+  name:string;
+  email: string;
+  password: string;
+}
+
+interface SignInParams {
+  email: string;
+  idToken: string;
+}
+
+interface User {
+  name: string;
+  email: string;
+  id: string;
+}
